da1aa97a44c22c7d514b648e2a00f24fe6391ece
Dangerise <dangerise@qq.com>
2025-02-10 20:57:37 +0800

    update

--- src/commands/judge.rs
+++ src/commands/judge.rs
@@ -1,45 +1,50 @@
 use clap::Parser;
-use std::ffi::OsStr;
-use std::fs::read_dir;
+use eyre::{Context, ContextCompat, Ok};
+use std::fs::{self};
 use std::path::PathBuf;
+
+use crate::workspace::Workspace;
+use crate::{compile, executable_path, judge, run};
 
 #[derive(Parser, Default)]
 pub struct JudgeArgs {
     #[clap(short, long)]
     only: Option<PathBuf>,
-    #[clap(default_value_t=String::from("./"))]
-    directory: String,
-    #[clap(short, long, default)]
-    source: String,
-    #[arg(value_name = "EXECUTABLE")]
-    executable: Option<String>,
 }
 
-pub fn judge(args: JudgeArgs) -> eyre::Result<()> {
-    let JudgeArgs { only, directory } = args;
-    let directory = PathBuf::from(directory);
+pub fn judge(workspace: Workspace, args: JudgeArgs) -> eyre::Result<()> {
+    let Workspace {
+        main_code,
+        mut data,
+        ..
+    } = workspace;
 
-    if only.is_some() {
-        assert!(
-            directory.as_os_str() != OsStr::new("./"),
-            "Directory and only should not be both set !"
-        );
-    }
+    let main_code = main_code.wrap_err("main code is not given !")?;
+    let JudgeArgs { only } = args;
+    if let Some(path) = only {
+        data = vec![path];
+    };
 
-    let mut tasks = Vec::new();
+    let executable = executable_path(&main_code)?;
+    compile::compile(&main_code, &executable)?;
 
-    for elm in read_dir(&directory)? {
-        let entry = elm?;
-        let path = entry.path();
-        if path.extension() == Some(OsStr::new("in")) {
-            let file_stem = path.file_stem().unwrap();
-            tasks.push(directory.join(file_stem));
+    for in_file in data {
+        let input = fs::read_to_string(&in_file)
+            .wrap_err_with(|| format!("Error occured while reading \"{}\"", in_file.display()))?;
+        println!("Running {}\n", in_file.display());
+        let result = run::run_silently(&executable, &input)?;
+        let out_file = in_file.with_extension("out");
+        if out_file.exists() {
+            let ans = fs::read_to_string(&out_file).wrap_err_with(|| {
+                format!("Error occured while reading \"{}\"", out_file.display())
+            })?;
+            if judge::judge(&result.output, &ans) {
+                println!("Passed\n");
+            } else {
+                println!("Failed !");
+                return Ok(());
+            }
         }
     }
-
-    for task in tasks {
-        let in_file = task;
-    }
-
     Ok(())
 }
--- src/commands/new.rs
+++ src/commands/new.rs
@@ -6,10 +6,6 @@ pub struct NewArgs {
     name: String,
     #[clap(short, long, value_name = "DIR", default_value_t=String::from("."))]
     directory: String,
-    #[clap(short, long, default_value_t = false)]
-    contest: bool,
-    #[clap(short, long, default_value_t = 4)]
-    problems_num: u32,
 }
 
 use std::fs;
@@ -33,36 +29,9 @@ fn new_problem(name: &str, parent_directory: &Path) -> eyre::Result<()> {
     Ok(())
 }
 
-fn new_contest(name: &str, parent_directory: &Path, problems_num: u32) -> eyre::Result<()> {
-    let directory = PathBuf::from(parent_directory).join(name);
-    fs::create_dir_all(&directory)?;
-
-    const CHAR_MAP: [char; 26] = [
-        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
-        'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-    ];
-    assert!(problems_num <= 26);
-
-    let mut buffer: [u8; 128] = [0; 128];
-    for problem in 0..problems_num {
-        let name = CHAR_MAP[problem as usize].encode_utf8(&mut buffer);
-        new_problem(name, &directory)?;
-    }
-    Ok(())
-}
-
 pub fn new(args: NewArgs) -> eyre::Result<()> {
-    let NewArgs {
-        name,
-        directory,
-        contest,
-        problems_num,
-    } = args;
+    let NewArgs { name, directory } = args;
     let directory = PathBuf::from(directory);
-    if contest {
-        new_contest(&name, &directory, problems_num)?;
-    } else {
-        new_problem(&name, &directory)?;
-    }
+    new_problem(&name, &directory)?;
     Ok(())
 }
